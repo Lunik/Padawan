52c09bd015ad6939305cf301f2a2966a8d240c8b
lunik <guillaume.lunik@gmail.com>
2018-08-21 21:37:33 +0200

    remove additional responses

--- padawanv6/lib/resolver.py
+++ padawanv6/lib/resolver.py
@@ -1,5 +1,5 @@
 from dnslib import QTYPE, RCODE
-from dnslib import NS, SOA, TXT, PTR
+from dnslib import PTR
 
 from .record import Record
 from .config import Config
@@ -14,15 +14,11 @@ class Resolver:
         reply = request.reply()
 
         if request.q.qtype == QTYPE.PTR and CONFIG.ipv6_subnet in str(request.q.qname):
-            for record in CONFIG.records['ok']:
-                reply.add_answer(record.as_rr(request.q.qname))
 
             ipv6_digits = str(request.q.qname).replace(CONFIG.ipv6_subnet, '').replace('.', '')[::-1]
             code = CONFIG.record_pattern.replace(CONFIG.DIGIT_TAG, ipv6_digits)
             reply.add_answer(Record(PTR, code).as_rr(request.q.qname))
         else:
             reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
-            for record in CONFIG.records['ko']:
-                reply.add_answer(record.as_rr(request.q.qname))
 
         return reply
